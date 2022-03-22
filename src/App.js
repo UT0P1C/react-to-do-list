3edd75108c4af5ffd950ce5530c918fc20faaa54
UT0P1C <ljonham@gmail.com>
2022-03-21 22:56:50 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+	const [tasks, setTasks] = useState([
+		/*{
+			id: 0,
+			task: "Study React",
+			checked: true
+		}*/
+	]);
+
+	const [modal, setModal] = useState(false);
+
+	const openModal = () => {
+		setModal(!modal);
+	}
+
+	const saveTask = () => {
+		alert("Task saved");
+	}
+
+	return (
+		<div className="App">
+
+			{
+				modal?
+					<div className="modal">
+						<div className="modalContent">
+							<h3>Add Task</h3>
+							<input type="text"/>
+							<button onClick={() => saveTask()}>Add</button>
+						</div>
+
+					</div>
+				:
+				<div></div>
+			}
+
+			<div onClick={() => openModal()} className="addTask">
+				+
+			</div>
+			<div className="taskBox">
+				<h3>My tasks for today!</h3>
+
+				{
+					tasks.map((val) => {
+						if(val.checked){
+							return (
+								<p>{val.task}</p>
+							);
+						}else{
+							return (
+								<p style={{textDecoration: 'line-through'}}>{val.task}</p>
+							);
+						}
+					})
+				}
+			</div>
+		</div>
+	);
 }
 
 export default App;
